660b0fa949e244bde87a8037b3b1bbebe7d3c65b
刘志强 <liuzhiqiang1@chinasie.com>
2025-11-27 09:04:31 +0800

    前端代码

---
+++ vue/admin/apps/web-auth/src/api/core/auth.ts
@@ -0,0 +1,54 @@
+import { baseRequestClient, requestClient } from '#/api/request';
+
+export namespace AuthApi {
+  /** 登录接口参数 */
+  export interface LoginParams {
+    password?: string;
+    username?: string;
+    userName?: string;
+  }
+
+  /** 登录接口返回值 */
+  export interface LoginResult {
+    access_token: string;
+  }
+
+  export interface RefreshTokenResult {
+    data: string;
+    status: number;
+  }
+}
+
+/**
+ * 登录
+ */
+export async function loginApi(data: AuthApi.LoginParams) {
+  data.userName = data.username;
+  return await requestClient.post<AuthApi.LoginResult>('/Account/Login', data);
+}
+
+/**
+ * 刷新accessToken
+ */
+export async function refreshTokenApi() {
+  return baseRequestClient.post<AuthApi.RefreshTokenResult>('/auth/refresh', {
+    withCredentials: true,
+  });
+}
+
+/**
+ * 退出登录
+ */
+export async function logoutApi() {
+  return requestClient.post('/Account/Logout', {
+    withCredentials: true,
+  });
+}
+
+/**
+ * 获取用户权限码
+ */
+export async function getAccessCodesApi() {
+  return [];
+  //return requestClient.get<string[]>('/auth/codes');
+}
